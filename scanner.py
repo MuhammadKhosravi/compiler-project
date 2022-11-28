7bdd316d1aef95b4a7a7e75e9cb8ec28cd1ed4d3
Artemis-Fowl1381 <arash13811393@gmail.com>
2022-11-28 17:24:20 +0330

    checked and think it is all right

--- scanner.py
+++ scanner.py
@@ -12,6 +12,17 @@ class State:
     def set_accepting(self):
         self.is_accepting = True
 
+    def __str__(self):
+        description = "state " + str(self.number) + "\ntransitions: \n"
+        for char, state in zip(self.characters, self.next_states):
+            description += "with " + char + " go to the state " + str(state.number) + "\n"
+        if self.is_accepting:
+            description += "it is final state"
+        return description
+
+    def __repr__(self):
+        return str(self)
+
 
 # d is used to represent digit
 # l is used to represent letter
@@ -44,13 +55,14 @@ class Scanner:
                              {0: 9, 9: 11, 14: 15},  # for /
                              {11: 12},  # for \n
                              {1: 2, 3: 4, 6: 7, 13: 13, 11: 11, 9: 10},  # for other
-                             {11: 12}] # for EOF
+                             {11: 12}]  # for EOF
         for char, states_tran in zip(characters, states_transition):
             for first, second in states_tran.items():
                 states[first].transition(states[second], char)
         final_states = [2, 4, 5, 7, 8, 16, 10, 12, 15]
         for st in final_states:
             states[st].set_accepting()
+        return states
 
     def get_next_token(self):
         current_state = self.states[0]
